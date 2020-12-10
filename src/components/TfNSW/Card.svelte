7cb78f95897fe275adfc4028a34445663dfd7fda
Andrew Wong <andrew.j.wong@outlook.com>
2020-12-10 17:35:18 +1100

    Transport card

--- src/components/TfNSW/Card.svelte
+++ src/components/TfNSW/Card.svelte
@@ -5,11 +5,19 @@
   export let data: ServiceInformation;
 </script>
 
-{data.routeDescription}
-
-{data.routeNumber}
-
-{minuteDifference(new Date(), data.departureTimeEstimated || data.departureTimePlanned)}
-min
-
-{#if data.delay}({data.delay} min {data.delay > 0 ? 'late' : 'early'}){/if}
+<div class="tile u-items-center">
+  <h5 class="mb-0" class:text-red-600={data.delay > 0} class:text-green-500={data.delay < 0} >
+    {minuteDifference(new Date(), data.departureTimeEstimated || data.departureTimePlanned)}
+    min
+  </h5>
+  <div class="tile__container">
+    <p class="tile__title m-0">{data.routeDescription} ({data.routeNumber})</p>
+    {#if data.delay}
+      <p class="tile__subtitle m-0">
+        {data.delay}
+        min
+        {data.delay > 0 ? 'late' : 'early'}
+      </p>
+    {/if}
+  </div>
+</div>
